94b59db285aa16efbfaefd2e9f6ef2748b297308
かえる <kasuo.takumi@gmail.com>
2021-05-14 09:50:38 +0900

    setup: any pages and router
    - ホーム画面
    - 新規登録画面
    - ログイン画面
    - プロフィール画面

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,23 @@
 <script lang="ts">
-const text: string = 'hello';
+import { Router, Link, Route } from 'svelte-routing';
 
+import Home from './lib/Home.svelte';
+import Signup from './lib/Signup.svelte';
+import Login from './lib/Login.svelte';
+import Profile from './lib/Profile.svelte';
+export let url = '';
 </script>
 
-<h1>{text}</h1>
+<Router url="{url}">
+  <div>
+    <!-- ホーム画面 -->
+    <Route path="/"><Home /></Route>
+    <!-- 新規登録 -->
+    <Route path="/signup"><Signup /></Route>
+    <!-- ログイン -->
+    <Route path="/login"><Login /></Route>
+    <!-- プロフィール -->
+    <Route path="/profile"><Profile /></Route>
+    <Route>not found</Route>
+  </div>
+</Router>
