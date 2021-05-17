d42b567be48825b29046de8d48d706e7b2babd83
Frog-kt <kasuo.takumi@gmail.com>
2021-05-18 00:14:45 +0900

    Update Login.svelte

--- client/src/lib/Login.svelte
+++ client/src/lib/Login.svelte
@@ -1,23 +1,45 @@
 <script lang="ts">
 import { Router, Link, Route, navigate } from 'svelte-routing';
 let username, password, error;
+let response = null;
+let promise = Promise.resolve([]);
+let disabled = false;
+let continents;
 
 const baseUrl = 'http://localhost:3030';
 
-const submit = async () => {
-  try {
-    await fetch(baseUrl + '/auth/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        username: username,
-        password: password,
-      }),
+// const submit = async () => {
+//   try {
+//     const hoge = await fetch(baseUrl + '/auth/login', {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify({
+//         username: username,
+//         password: password,
+//       }),
+//     });
+
+//     response = hoge.body.getReader();
+
+//     console.log(hoge);
+//   } catch (err) {
+//     console.log(err);
+//     error = err.message;
+//   }
+// };
+
+const fetchData = async () => {
+  await fetch(baseUrl + '/auth/test')
+    .then(r => r.json())
+    .then(data => {
+      continents = data;
     });
-  } catch (err) {
-    console.log(err);
-    error = err.message;
-  }
+};
+
+const handleClick = () => {
+  promise = fetchData();
+  console.log(promise);
+  disabled = true;
 };
 
 </script>
@@ -32,9 +54,16 @@ const submit = async () => {
 <div class="w-200">
   <input type="text" bind:value="{username}" class="border-1px" placeholder="username" />
   <input type="password" bind:value="{password}" class="border-1px" placeholder="password" />
-  <button on:click="{submit}">Login</button>
+  <button on:click="{handleClick}" disabled="{disabled}">Login</button>
 </div>
 
-{#if error}
-  <p>{error}</p>
-{/if}
+{#await promise}
+  <p>...waiting</p>
+{:then users}
+  {#each users as { login, url } (login)}
+    <h3>{login}</h3>
+    <img height="100" src="{genericAvatar}" alt="{login}" use:fetchAvatar="{url}" />
+  {/each}
+{:catch error}
+  <p style="color: red">{error.message}</p>
+{/await}
