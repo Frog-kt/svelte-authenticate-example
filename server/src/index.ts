31537e064d647b5fe49479f92d8dfa7933a99d38
Frog-kt <kasuo.takumi@gmail.com>
2021-05-18 00:29:20 +0900

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ import { registerRoutes } from './routes'
 
 const app = express()
 
-app.use(cors())
+app.use(cors({ origin: 'http://localhost:3000' }))
 app.use(express.json())
 app.use(cookieParser())
 app.use(morgan('tiny'))
