a767052888ce9c26bdc0173d97330fda6ac27126
Frog-kt <kasuo.takumi@gmail.com>
2021-05-17 21:08:06 +0900

    Update example.middleware.ts

--- server/src/middlewares/example.middleware.ts
+++ server/src/middlewares/example.middleware.ts
@@ -1,15 +1,17 @@
-import { parseSecureToken } from '../libs/auth';
+import { parseSecureToken } from '../libs/auth'
 import { Request, Response, NextFunction } from 'express'
 
-export function exampleMiddleware() { }
+export function exampleMiddleware() {}
 
 export async function isAuth(req: Request, res: Response, next: NextFunction) {
-    const token = req.cookies[process.env.AUTH_COOKIE_NAME];
-    if (!token) res.status(401).json({ success: false });
+  console.log(req)
+  const token = req.cookies["Bearer"]
+  //   console.log(token)
+  if (!token) res.status(401).json({ success: false })
 
-    const authUser = await parseSecureToken(token);
-    if (!authUser) res.status(401).json({ success: false });
+  const authUser = await parseSecureToken(token)
+  if (!authUser) res.status(401).json({ success: false })
 
-    req.user = authUser.userId;
-    next();
+  req.user = authUser.userId
+  next()
 }
