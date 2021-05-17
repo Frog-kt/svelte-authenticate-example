05e1cf871010739bdd2a86636ba3027da66f9401
Frog-kt <kasuo.takumi@gmail.com>
2021-05-17 09:07:24 +0900

    Update Login.svelte

--- client/src/lib/Login.svelte
+++ client/src/lib/Login.svelte
@@ -1,37 +1,6 @@
 <script lang="ts">
-let password = '';
-let email = '';
-let error;
-
-const handleLogin = async () => {
-  const response = await fetch('/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-    body: JSON.stringify({ email, password }),
-  });
-};
-
 </script>
 
 <div>
   <h1>Login</h1>
 </div>
-
-<form on:submit|preventDefault="{handleLogin}" method="post">
-  <label>
-    Email:
-    <input type="email" bind:value="{email}" />
-  </label>
-  <label>
-    Password:
-    <input type="password" bind:value="{password}" />
-  </label>
-  <button type="submit">Login</button>
-</form>
-
-{#if error}
-  <p>{error}</p>
-{/if}
