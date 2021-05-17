c1033668a020cb5dc03828cba43562a99cef25d7
Frog-kt <kasuo.takumi@gmail.com>
2021-05-18 00:14:54 +0900

    Update auth.route.ts

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,11 +1,12 @@
 import { isAuth } from '@/middlewares'
 import { Router } from 'express'
-import { signup, login, me } from '../controllers'
+import { signup, login, me, test } from '../controllers'
 
 const router = Router()
 
 router.post('/signup', signup)
 router.post('/login', login)
 router.get('/me', isAuth, me)
+router.get('/test', test)
 
 export default router
