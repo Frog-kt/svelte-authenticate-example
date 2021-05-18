613862043d3f27d4e9e7878aa90ccfe27125830b
かえる <kasuo.takumi@gmail.com>
2021-05-18 12:35:45 +0900

    Update Profile.svelte

--- client/src/lib/Profile.svelte
+++ client/src/lib/Profile.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+import { Router, Link, Route } from 'svelte-routing';
 const baseUrl = 'http://localhost:3030';
 let promise;
 
@@ -14,14 +15,18 @@ const login = async () => {
 </script>
 
 <div>
-  <h1>Profile</h1>
-  <p>
-    {#await login()}
-      <p>...waiting</p>
-    {:then data}
-      <p>{JSON.stringify(data)}</p>
-    {:catch error}
-      <p style="color: red">{error.message}</p>
-    {/await}
-  </p>
+  <Link to="/">Home</Link>
+  <Link to="/signup">Signup</Link>
+  <Link to="/login">Login</Link>
+  <Link to="/profile">Profile</Link>
 </div>
+<h1>Profile</h1>
+<p>
+  {#await login()}
+    <p>...waiting</p>
+  {:then data}
+    <p>{JSON.stringify(data)}</p>
+  {:catch error}
+    <p style="color: red">{error.message}</p>
+  {/await}
+</p>
