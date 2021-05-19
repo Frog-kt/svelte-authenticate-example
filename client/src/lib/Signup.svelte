721409fc8cd6e87914bd056451c8b24315436da1
Frog-kt <kasuo.takumi@gmail.com>
2021-05-19 22:57:00 +0900

    chore

--- client/src/lib/Signup.svelte
+++ client/src/lib/Signup.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 </script>
+
 <div>
   <Link to="/">Home</Link>
   <Link to="/signup">Signup</Link>
   <Link to="/login">Login</Link>
   <Link to="/profile">Profile</Link>
 </div>
-  <h1>Signup</h1>
+<h1>Signup</h1>
